816ba3e1a3f81fde11ef6013cb8933c235989548
Chris Knoll <cknoll3@gmail.com>
2016-05-27 05:15:54 -0700

    Added Modal package and template.  Bar actions now check for money, use modal template to display nice result message including stat changes.  Added additional flavor text.  Removed unused images from public.

--- client/js/bar.js
+++ client/js/bar.js
@@ -1,22 +1,39 @@
-/*
-Doesn't work... Going to static buttons
-Template.bar.helpers({
-  getActions: function() {
-    return Meteor.call('getActions', 'bar');
-  }
-});*/
-
 Template.bar.events({
   'click #buyDrink': function(event) {
-    alert("You bought a drink");
-    Meteor.call('incRank', Meteor.userId(), 1);
-    Meteor.call('incMoney', Meteor.userId(), -5);
+    if (Meteor.user().profile.money >= 5) {
+      Modal.show('modal', {
+        'title': 'Bought a Drink',
+        'message': 'You bought a drink.  Obviously drinking is cool, so your social rank goes up slightly.',
+        'socialRank': '+1',
+        'money': '-5',
+      });
+      Meteor.call('incRank', Meteor.userId(), 1);
+      Meteor.call('incMoney', Meteor.userId(), -5);
+    } else {
+      Modal.show('modal', {
+        'title': 'Embarrassing...',
+        'message': "You order a drink, but realize you don't have enough cash to pay for it.  You have to call your mom to come cover your bill.  Not only does the bartender shake his head in disgust, everyone at the bar thinks you're a little less cool.",
+        'socialRank': '-2',
+      });
+      Meteor.call('incRank', Meteor.userId(), -2);
+    }
   },
 
   'click #buyRound': function(event) {
-
-    alert("You buy a round of drinks for the bar.");
-    Meteor.call('incRank', Meteor.userId(), 5);
-    Meteor.call('incMoney', Meteor.userId(), -30);
+    if (Meteor.user().profile.money >= 30) {
+      Modal.show('modal', {
+        'title': 'Bought a Round',
+        'message': 'You buy a round of drinks for the bar.  Everyone at the bar likes you!'
+      });
+      Meteor.call('incRank', Meteor.userId(), 5);
+      Meteor.call('incMoney', Meteor.userId(), -30);
+    } else {
+      Modal.show('modal', {
+        'title': 'Embarrassing...',
+        'message': "You order a round of drinks for the bar, but realize you don't have enough cash to pay for it.  Everyone has to pay for their 'free' drink.  No one likes you right now.",
+        'socialRank': '-5',
+      });
+      Meteor.call('incRank', Meteor.userId(), -5);
+    }
   }
 });
