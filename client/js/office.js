7f7fac5607f17a1a3d38d88c425e1b4e30591ea7
Chris Knoll <cknoll3@gmail.com>
2016-05-28 18:03:14 -0700

    Switched office actions to modals.

--- client/js/office.js
+++ client/js/office.js
@@ -1,20 +1,22 @@
 Template.office.events({
   'click #meteorWork': function(event) {
-    alert("You spend 10 hours trying to get your Mongo collections to work on your Meteor project, but make no progress.  Your boss makes you give him all the money in your wallet to pay for your failure.");
-    Meteor.call('incMoney', Meteor.userId(), -5);
+    // Get current money
+    var currentMoney = Meteor.user().profile.money;
+    Modal.show('modal', {
+      'title': 'Work...?',
+      'message': "You spend 10 hours trying to get your Mongo collections to work on your Meteor project, but make no progress.  Your boss makes you give him all the money in your wallet to pay for your failure.",
+      'money': '-' + currentMoney
+    });
+    Meteor.call('incMoney', Meteor.userId(), -currentMoney);
   },
 
   'click #stealCode': function(event) {
-    alert("You steal your coworkers Meteor code and submit it to your boss as your own.  Your boss loves it and gives you a bonus!  Your coworker finds out, and likes you less than they did before.");
+    Modal.show('modal', {
+      'message': "You steal your coworkers Meteor code and submit it to your boss as your own.  Your boss loves it and gives you a bonus!  Your coworker finds out, and likes you less than they did before.",
+      'money': '+40',
+      'socialRank': '-1'
+    });
     Meteor.call('incMoney', Meteor.userId(), 40);
     Meteor.call('incRank', Meteor.userId(), -1);
   }
 });
-
-/*
-Template.office.helpers({
-  getActions: function() {
-    return Meteor.call('getActions', 'office');
-  }
-});
-*/
