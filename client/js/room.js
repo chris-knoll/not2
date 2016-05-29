db51d7b41b73f120ce871e17968d64bffe64145c
Chris Knoll <cknoll3@gmail.com>
2016-05-29 05:13:30 -0700

    Updated room actions to modals.

--- client/js/room.js
+++ client/js/room.js
@@ -1,20 +1,30 @@
 Template.room.events({
   'click #rest': function(event) {
-    alert("You try to rest but you can't stop thinking about things to possibly try to get your Meteor project working.");
+    Modal.show('modal', {
+      'title': 'Rest',
+      'message': "You try to rest but you can't stop thinking about things to possibly try to get your Meteor project working.",
+      'stamina': '+1'
+    });
     Meteor.call('incStamina', Meteor.userId(), 1);
   },
 
   'click #sleep': function(event) {
-    alert("You try to sleep but you can't stop thinking about Meteor and why you can't get it to work.  You end up going to the store to buy sleeping pills, take them, and sleep for 2 hours.");
-    Meteor.call('incStamina', Meteor.userId(), 2);
-    Meteor.call('incMoney', Meteor.userId(), -5);
+    if (Meteor.user().profile.money >= 5) {
+      Modal.show('modal', {
+        'title': 'Sleep',
+        'message': "You try to sleep but you can't stop thinking about Meteor and why you can't get it to work.  You end up going to the store to buy sleeping pills, take them, and sleep for 5 hours.",
+        'stamina': '+5',
+        'money': '-5'
+      });
+      Meteor.call('incStamina', Meteor.userId(), 5);
+      Meteor.call('incMoney', Meteor.userId(), -5);
+    } else {
+      Modal.show('modal', {
+        'title': 'Sleep',
+        'message': "You try to sleep but you can't stop thinking about Meteor and why you can't get it to work.  You toss and turn, getting a little rest.",
+        'stamina': '+2'
+      });
+      Meteor.call('incStamina', Meteor.userId(), 2);
+    }
   }
 });
-
-/*
-Template.room.helpers({
-  getActions: function() {
-    return Meteor.call('getActions', 'room');
-  }
-});
-*/
