8b965f28b239bcd938238e67bcb1739b82a813bd
Chris Knoll <cknoll3@gmail.com>
2016-05-30 21:39:40 -0700

    Updated class actions to modals.

--- client/js/class.js
+++ client/js/class.js
@@ -1,16 +1,29 @@
 Template.class.events({
   'click #attendClass': function(event) {
-    alert("You attend class and are shown an example of how to get Collections working in Mongo.  You are super excited to try it at home.  You try to mimic the code in your own project, but it fails to run.");
+    Modal.show('modal', {
+      'title': 'Class',
+      'message': "You attend class and are shown an example of how to get Collections working in Mongo.  You are super excited to try it at home.  You try to mimic the code in your own project, but it fails to run.",
+      'stamina': '-1',
+    });
     Meteor.call('incStamina', Meteor.userId(), -1);
   },
 
   'click #studyAlone': function(event) {
-    alert("You attempt to study alone, but get stuck within 30 minutes of starting.  You play video games for 5 hours instead.");
-    Meteor.call('incStamina', Meteor.userId(), -1);
+    Modal.show('modal', {
+      'title': 'Study',
+      'message': "You attempt to study alone, but get stuck within 30 minutes of starting.  You play video games for 5 hours instead.",
+      'stamina': '-5'
+    });
+    Meteor.call('incStamina', Meteor.userId(), -5);
   },
 
   'click #socialize': function(event) {
-    alert("You go to class, but instead of paying attention, you socialize with your friends.  You don't learn anything, but people think you are a little bit cooler.");
+    Modal.show('modal', {
+      'title': 'Socialize',
+      'message': "You go to class, but instead of paying attention, you socialize with your friends.  You don't learn anything, but people think you are a little bit cooler.",
+      'stamina': '-1',
+      'socialRank': '+1'
+    });
     Meteor.call('incRank', Meteor.userId(), 1);
     Meteor.call('incStamina', Meteor.userId(), -1);
   }
