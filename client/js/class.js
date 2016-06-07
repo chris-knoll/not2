ac6b469dec1af8a9df9d3e38c5e9815a2f5752d2
Chris Knoll <cknoll3@gmail.com>
2016-06-07 16:16:19 -0700

    Added checks to see if user has enough stamina to perform actions that cost stamina.

--- client/js/class.js
+++ client/js/class.js
@@ -1,43 +1,73 @@
 Template.class.events({
   'click #attendClass': function(event) {
-    Modal.show('modal', {
-      'title': 'Class',
-      'message': "You attend class and are shown an example of how to get Collections working in Mongo.  You are super excited to try it at home.  You try to mimic the code in your own project, but it fails to run.",
-      'stamina': '-1',
-      'intelligence': '+5',
-      'money': '+0',
-      'socialRank': '+0'
-    });
-    Meteor.call('incStamina', Meteor.userId(), -1);
-    Meteor.call('incIntelligence', Meteor.userId(), 5);
-    Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 1);}, 600000);
+    if (Meteor.user().profile.stamina < 1) {
+      Modal.show('modal', {
+        'title': 'Too tired...',
+        'message': 'You are too tired to perform that action.. perhaps you should go rest?',
+        'socialRank': '+0',
+        'money': '+0',
+        'stamina': '+0'
+      });
+    } else {
+      Modal.show('modal', {
+        'title': 'Class',
+        'message': "You attend class and are shown an example of how to get Collections working in Mongo.  You are super excited to try it at home.  You try to mimic the code in your own project, but it fails to run.",
+        'stamina': '-1',
+        'intelligence': '+5',
+        'money': '+0',
+        'socialRank': '+0'
+      });
+      Meteor.call('incStamina', Meteor.userId(), -1);
+      Meteor.call('incIntelligence', Meteor.userId(), 5);
+      Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 1);}, 10000);
+    }
   },
 
   'click #studyAlone': function(event) {
-    Modal.show('modal', {
-      'title': 'Study',
-      'message': "You attempt to study alone, but get stuck within 30 minutes of starting.  You play video games for 5 hours instead.",
-      'stamina': '-5',
-      'intelligence': '+1',
-      'money': '+0',
-      'socialRank': '+0'
-    });
-    Meteor.call('incStamina', Meteor.userId(), -5);
-    Meteor.call('incIntelligence', Meteor.userId(), 1);
-    Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 5);}, 3000000);
+    if (Meteor.user().profile.stamina < 5) {
+      Modal.show('modal', {
+        'title': 'Too tired...',
+        'message': 'You are too tired to perform that action.. perhaps you should go rest?',
+        'socialRank': '+0',
+        'money': '+0',
+        'stamina': '+0'
+      });
+    } else {
+      Modal.show('modal', {
+        'title': 'Study',
+        'message': "You attempt to study alone, but get stuck within 30 minutes of starting.  You play video games for 5 hours instead.",
+        'stamina': '-5',
+        'intelligence': '+1',
+        'money': '+0',
+        'socialRank': '+0'
+      });
+      Meteor.call('incStamina', Meteor.userId(), -5);
+      Meteor.call('incIntelligence', Meteor.userId(), 1);
+      Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 5);}, 10000);
+    }
   },
 
   'click #socialize': function(event) {
-    Modal.show('modal', {
-      'title': 'Socialize',
-      'message': "You go to class, but instead of paying attention, you socialize with your friends.  You don't learn anything, but people think you are a little bit cooler.",
-      'stamina': '-1',
-      'intelligence': '+0',
-      'money': '+0',
-      'socialRank': '+1'
-    });
-    Meteor.call('incRank', Meteor.userId(), 1);
-    Meteor.call('incStamina', Meteor.userId(), -1);
-    Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 1);}, 600000);
+    if (Meteor.user().profile.stamina < 1) {
+      Modal.show('modal', {
+        'title': 'Too tired...',
+        'message': 'You are too tired to perform that action.. perhaps you should go rest?',
+        'socialRank': '+0',
+        'money': '+0',
+        'stamina': '+0'
+      });
+    } else {
+      Modal.show('modal', {
+        'title': 'Socialize',
+        'message': "You go to class, but instead of paying attention, you socialize with your friends.  You don't learn anything, but people think you are a little bit cooler.",
+        'stamina': '-1',
+        'intelligence': '+0',
+        'money': '+0',
+        'socialRank': '+1'
+      });
+      Meteor.call('incRank', Meteor.userId(), 1);
+      Meteor.call('incStamina', Meteor.userId(), -1);
+      Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 1);}, 10000);
+    }
   }
 });
--- client/js/office.js
+++ client/js/office.js
@@ -1,55 +1,73 @@
 Template.office.events({
   'click #meteorWork': function(event) {
-    //get user intelligence
-    var userIntelligence = Meteor.user().profile.intelligence;
-    // Get money
-    var earnedMoney = Math.floor((Math.random() * userIntelligence) + 5);
-    Modal.show('modal', {
-      'title': 'Work...?',
-      'message': "You spend several hours trying to code your Meteor project, but make no progress. Your boss is not pleased with your progress and tells you to work hard.",
-      'money': '+' + earnedMoney,
-      'stamina': '-2',
-      'socialRank': '+0'
-    });
-    Meteor.call('incMoney', Meteor.userId(), earnedMoney);
-    Meteor.call('incStamina', Meteor.userId(), -2);
-    Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 2);}, 1200000);
-
+    if (Meteor.user().profile.stamina < 2) {
+      Modal.show('modal', {
+        'title': 'Too tired...',
+        'message': 'You are too tired to perform that action.. perhaps you should go rest?',
+        'socialRank': '+0',
+        'money': '+0',
+        'stamina': '+0'
+      });
+    } else {
+      //get user intelligence
+      var userIntelligence = Meteor.user().profile.intelligence;
+      // Get money
+      var earnedMoney = Math.floor((Math.random() * userIntelligence) + 5);
+      Modal.show('modal', {
+        'title': 'Work...?',
+        'message': "You spend several hours trying to code your Meteor project, but make no progress. Your boss is not pleased with your progress and tells you to work hard.",
+        'money': '+' + earnedMoney,
+        'stamina': '-2',
+        'socialRank': '+0'
+      });
+      Meteor.call('incMoney', Meteor.userId(), earnedMoney);
+      Meteor.call('incStamina', Meteor.userId(), -2);
+      Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 2);}, 50000);
+    }
   },
 
   'click #stealCode': function(event) {
-    //get user intelligence
-    var userIntelligence = Meteor.user().profile.intelligence;
-    //getting money
-    var earnedMoney = Math.floor((Math.random() * userIntelligence) + 20);
-    var failedTheft = Meteor.user().profile.money;
-    var thiefStatus = Meteor.user().profile.socialRank;
-    console.log(thiefStatus);
-    //get caught or not
-    var stealingChance = Math.floor((Math.random() * userIntelligence) + 1);
-    var stealingBeat = Math.floor((Math.random() * userIntelligence) + 3);
-    if(stealingChance > stealingBeat){
+    if (Meteor.user().profile.stamina < 3) {
       Modal.show('modal', {
-        'message': "You steal your coworkers Meteor code and submit it to your boss as your own. Your boss loves it and gives you a bonus!",
-        'money': '+' + earnedMoney,
+        'title': 'Too tired...',
+        'message': 'You are too tired to perform that action.. perhaps you should go rest?',
         'socialRank': '+0',
-        'stamina': '-3'
+        'money': '+0',
+        'stamina': '+0'
       });
-      Meteor.call('incMoney', Meteor.userId(), earnedMoney);
-      Meteor.call('incRank', Meteor.userId(), 0);
-      Meteor.call('incStamina', Meteor.userId(), -3);
-      Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 3);}, 1800000);
-    }else {
-      Modal.show('modal', {
-        'message': "You attempted to steal your coworker's work, but got caught in the process. The police were called and you were made to pay all your money to get out of jail. Naturally your social status in society has gone down the drain.",
-        'money': '-' + failedTheft,
-        'socialRank': "Your rank is 0",
-        'stamina': '-3'
-      });
-      Meteor.call('setRank', Meteor.userId());
-      Meteor.call('incMoney', Meteor.userId(), -failedTheft);
-      Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 3);}, 1800000);
+    } else {
+      //get user intelligence
+      var userIntelligence = Meteor.user().profile.intelligence;
+      //getting money
+      var earnedMoney = Math.floor((Math.random() * userIntelligence) + 20);
+      var failedTheft = Meteor.user().profile.money;
+      var thiefStatus = Meteor.user().profile.socialRank;
+      console.log(thiefStatus);
+      //get caught or not
+      var stealingChance = Math.floor((Math.random() * userIntelligence) + 1);
+      var stealingBeat = Math.floor((Math.random() * userIntelligence) + 3);
+      if(stealingChance > stealingBeat){
+        Modal.show('modal', {
+          'message': "You steal your coworkers Meteor code and submit it to your boss as your own. Your boss loves it and gives you a bonus!",
+          'money': '+' + earnedMoney,
+          'socialRank': '+0',
+          'stamina': '-3'
+        });
+        Meteor.call('incMoney', Meteor.userId(), earnedMoney);
+        Meteor.call('incRank', Meteor.userId(), 0);
+        Meteor.call('incStamina', Meteor.userId(), -3);
+        Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 3);}, 50000);
+      }else {
+        Modal.show('modal', {
+          'message': "You attempted to steal your coworker's work, but got caught in the process. The police were called and you were made to pay all your money to get out of jail. Naturally your social status in society has gone down the drain.",
+          'money': '-' + failedTheft,
+          'socialRank': "Your rank is 0",
+          'stamina': '-3'
+        });
+        Meteor.call('setRank', Meteor.userId());
+        Meteor.call('incMoney', Meteor.userId(), -failedTheft);
+        Meteor.setTimeout(function(){Meteor.call('incStamina', Meteor.userId(), 3);}, 50000);
+      }
     }
-
   }
 });
